0b576434b6ee8e845839df2e826ed9ae297b25fe
Manirajkumar1 <manirajsingh7667@gmail.com>
2025-05-24 11:23:03 +0530

    update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,11 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import ThreeSpotlight from "../components/ThreeSpotlight";
+import Schedule from "../components/Schedule";
+import Timeline from "../components/Timeline";
+import ContentFeed from "../components/ContentFeed";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 const Homepage = () => {
   return (
@@ -8,6 +13,11 @@ const Homepage = () => {
       <Box w={"100%"} h={"92vh"}>
         <ThreeSpotlight />
       </Box>
+      <Schedule/>
+      <Timeline/>
+      <ContentFeed/>
+      <Contact/>
+      <Footer/>
     </Box>
   );
 };
