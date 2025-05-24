9c97aa036ed71aa5dc621ab6e72e549d399110e9
Manirajkumar1 <manirajsingh7667@gmail.com>
2025-05-24 11:21:37 +0530

    update with navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,45 +9,56 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Link as ScrollLink } from "react-scroll";
 
-const NavLink = (props) => {
-  const { children } = props;
-
+const NavLink = ({ to, children }) => {
   return (
-    <Box
-      as="a"
-      px={2}
-      py={1}
-      rounded={"md"}
-      _hover={{
-        textDecoration: "none",
-        bg: useColorModeValue("gray.200", "gray.700"),
-      }}
-      href={"#"}
+    <ScrollLink
+      to={to}
+      smooth={true}
+      duration={500}
+      offset={-70} // Adjust if your navbar is sticky
+      spy={true}
     >
-      {children}
-    </Box>
+      <Box
+        px={2}
+        py={1}
+        rounded={"md"}
+        cursor="pointer"
+        _hover={{
+          textDecoration: "none",
+          bg: useColorModeValue("gray.200", "gray.700"),
+        }}
+      >
+        {children}
+      </Box>
+    </ScrollLink>
   );
 };
 
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  return (
-    <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-        <Flex h={"8vh"} alignItems={"center"} justifyContent={"space-between"}>
-          <Text fontSize={'18px'} fontWeight={600}>NEUSTAR</Text>
 
-          <Flex alignItems={"center"}>
-            <Stack direction={"row"} spacing={7}>
-              <Button onClick={toggleColorMode}>
-                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-              </Button>
-            </Stack>
-          </Flex>
+  return (
+    <Box bg={useColorModeValue("gray.100", "gray.900")} px={4} position="sticky" top="0" zIndex="100">
+      <Flex h={"8vh"} alignItems={"center"} justifyContent={"space-between"}>
+        <Text fontSize={"18px"} fontWeight={600}>
+          NEUSTAR
+        </Text>
+
+        <Flex alignItems={"center"}>
+          <Stack direction={"row"} spacing={7} alignItems="center">
+            <NavLink to="home">Home</NavLink>
+            <NavLink to="blog">Blog</NavLink>
+            <NavLink to="contact">Contact</NavLink>
+
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            </Button>
+          </Stack>
         </Flex>
-      </Box>
-    </>
+      </Flex>
+    </Box>
   );
 }
